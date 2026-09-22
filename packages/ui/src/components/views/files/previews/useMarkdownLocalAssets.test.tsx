0a35e105f0b95cf7d2107dd2b3d206c127b44534
bashrusakh <bashrusakh@users.noreply.github.com>
2026-09-22 16:56:48 +1100

    test(ui): make markdown asset preview flush deterministic

--- packages/ui/src/components/views/files/previews/useMarkdownLocalAssets.test.tsx
+++ packages/ui/src/components/views/files/previews/useMarkdownLocalAssets.test.tsx
@@ -28,17 +28,17 @@ function Preview({ container, filePath, onOpenFile, enabled }: {
   return null;
 }
 
-const flush = async () => {
-  await act(async () => {
-    await new Promise((resolve) => setTimeout(resolve, 0));
-  });
-};
-
 describe('useMarkdownLocalAssets', () => {
   let windowInstance: Window;
   let root: Root;
   let container: HTMLDivElement;
   let created: string[];
+
+  const flush = async () => {
+    await act(async () => {
+      await windowInstance.happyDOM.waitUntilComplete();
+    });
+  };
 
   beforeEach(() => {
     windowInstance = new Window();
